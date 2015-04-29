319c1664adb4ac3d8c563f01db73ffe5d331c41d
Jeffery He <hejicode@gmail.com>
2015-04-29 14:05:51 +0800

    modify copyright
    modify copyright

--- HJCountDownButton/Classes/HJCountDownButton/HJCountDownButton.h
+++ HJCountDownButton/Classes/HJCountDownButton/HJCountDownButton.h
@@ -6,7 +6,7 @@
 
 // This code is distributed under the terms and conditions of the MIT license.
 //
-// Copyright (c) 2013 isaacl.net. All rights reserved.
+// Copyright (c) 2015 Jeffery He(hejicode@gmail.com). All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
